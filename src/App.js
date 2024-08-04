a1c988256e5591fdd96430673831540a3ae8afb0
satyamrkushwaha <satyamkushwahasrk95@gmail.com>
2024-08-04 13:00:14 +0530

    Simple Ui Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import StringCalculatorComponent from './components/StringCaculatorComp';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <StringCalculatorComponent />
       </header>
     </div>
   );
---
+++ src/components/StringCaculatorComp.js
@@ -0,0 +1,33 @@
+// src/StringCalculatorComponent.js
+import React, { useState } from 'react';
+
+
+const StringCalculatorComponent = () => {
+    const [input, setInput] = useState('');
+
+    const handleChange = (event) => {
+        setInput(event.target.value);
+    };
+
+   
+
+    return (
+        <div>
+            <form>
+                <textarea
+                    value={input}
+                    onChange={handleChange}
+                    rows="4"
+                    cols="50"
+                    placeholder="Enter numbers separated by commas or new lines"
+                />
+                <br />
+                <button type="submit">Calculate</button>
+            </form>
+            <p>Result:</p>
+         <p style={{color: 'red'}}>Error:</p>
+        </div>
+    );
+};
+
+export default StringCalculatorComponent;
